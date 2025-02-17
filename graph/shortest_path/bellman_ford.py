b22e95d8313a7c074737053617b6d105d0dce905
eduardburlacu <efb48@cam.ac.uk>
2025-02-17 16:49:47 +0000

    negative cycle det

--- graph/shortest_path/bellman_ford.py
+++ graph/shortest_path/bellman_ford.py
@@ -1,6 +1,9 @@
 from typing import Dict, List, Set, Tuple
 
-def shortest_paths(graph:Dict[int, Set[Tuple[int,float]]], source:int):
+def bellman_ford(
+        graph:Dict[int, Set[Tuple[int,float]]], source:int,
+        detect_negative_cycle:bool=False
+)-> Tuple[List[int], List[float]]:
     V=len(graph)
     dist = [float("inf")]*V
     dist[source] = 0.
@@ -12,6 +15,13 @@ def shortest_paths(graph:Dict[int, Set[Tuple[int,float]]], source:int):
                 if dist[v] > target:
                     dist[v] = target
                     ps[v] = u
+
+    if detect_negative_cycle:
+        for u in range(V):
+            for v, w in graph[u]:
+                if dist[v] > dist[u] + w:
+                    raise ValueError("Negative Cycle Detected")
+        print("No negative cycle detected")
     return ps, dist
 
 def backtrack_path(parents:List[int], target:int):
@@ -32,11 +42,12 @@ if __name__=="__main__":
         4: {(0, 1), (3, 3)}
     }
     source = 0
-    parents, dists = shortest_paths(graph, source)
     print(f"Graph: {graph}")
+    parents, dists = bellman_ford(graph, source, detect_negative_cycle=True)
     print(f"Parents: {parents}")
     print(f"Dists: {dists}")
     for target in range(len(graph)):
+        path = backtrack_path(parents, target)
         print(
-            f"Path from {source} to {target}: {backtrack_path(parents, target)} with cost {dists[target]}"
+            f"Path from {source} to {target}: {path} with cost {dists[target]}"
         )
